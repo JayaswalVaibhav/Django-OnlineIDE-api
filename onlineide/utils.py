267e103420a128e8e60d0b42ea98420cf0863618
JayaswalVaibhav <vaibhavjayaswal4180@gmail.com>
2021-12-08 23:44:20 +0530

    added java support

--- onlineide/utils.py
+++ onlineide/utils.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 import uuid
 import django
@@ -19,6 +20,28 @@ def execute_file(filename, language, submission_id):
     submission = SubmissionCode.objects.get(pk=submission_id)
     if language == "py":
         result = subprocess.run(["python", "./code/" + filename], stdout=subprocess.PIPE)
+        if result.returncode != 0:
+            # runtime error
+            submission.status = "E"
+            submission.save()
+            return
+        submission.user_output = result.stdout.decode("utf-8").strip()
+        submission.status = "S"
+        submission.save()
+
+    elif language == "java":
+        # this will create a folder with name - filename
+        result = subprocess.run(["javac", "./code/"+filename, "-d", "./code/"+filename.split('.')[0]],
+                                stdout=subprocess.PIPE)
+        if result.returncode != 0:
+            # runtime error
+            submission.status = "E"
+            submission.save()
+            return
+
+        class_name = os.listdir("./code/"+filename.split('.')[0]+"/")[0].split('.')[0]
+        result = subprocess.run(["java", "-cp", "./code/"+filename.split('.')[0], class_name],
+                                stdout=subprocess.PIPE)
         if result.returncode != 0:
             # runtime error
             submission.status = "E"
